a48d760552bd22fc4f19831b096b53f50b314203
Václav Stehlík <vaclav.stehlik@devix.cz>
2020-06-09 20:02:49 +0200

    Improve handling of criteria

--- main.py
+++ main.py
@@ -7,11 +7,46 @@ from skcriteria import Data, MAX
 from skcriteria.madm import closeness
 
 
-def main():
-    user_criteria = json.loads(sys.argv[1])
+class RankingResponse:
+    def __init__(self, alternative_name, rank):
+        self.alternative_name = alternative_name
+        self.rank = rank
 
-    weights = {k: criteria_preference_to_weight(v) for k, v in
-               user_criteria["user_criteria"]["question_big_data"][0].items()}
+
+CRITERIA_DEFAULT = {
+    "volume": "0",
+    "variety": "0",
+    "velocity": "0",
+    "temporal": "0",
+    "one_ts": "0",
+    "multiple_ts": "0",
+    "categoric": "0",
+    "numeric": "0",
+    "mixed_categoric_numeric": "0",
+    "geospatial": "0",
+    "hierarchical": "0",
+    "network": "0",
+    "distribution": "0",
+    "flow": "0",
+    "performance": "0",
+    "composition": "0",
+    "space_efficiency": "0",
+    "range": "0",
+    "outliers": "0",
+    "process_modeling": "0",
+    "relationships": "0",
+    "comparison": "0",
+    "negative_values": "0",
+    "number_of_variables": "0",
+    "relative_proportions": "0"
+}
+
+
+def main():
+    user_criteria_raw = json.loads(sys.argv[1])
+    user_criteria = {k: criteria_preference_to_weight(v) for k, v in list(extract_criteria(user_criteria_raw))}
+    criteria = update_criteria(user_criteria)
+    weights = list(criteria.values())
 
     with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'rating_matrix.json')) as f:
         data = json.load(f)
@@ -28,13 +63,33 @@ def main():
     dm = closeness.TOPSIS()
     dec = dm.decide(data)
 
-    ranking = {"ranking": dict(zip(alternative_names, dec.rank_.tolist()))}
+    ranking = export_ranking(alternative_names, dec.rank_.tolist())
     print(json.dumps(ranking))
 
 
 def criteria_preference_to_weight(value):
-    weight_ratio = {'1': 0, '2': 0.25, '3': 0.5, '4': 0.75, '5': 1}
+    weight_ratio = {'True': 1, 'False': 0, '1': 0, '2': 0.25, '3': 0.5, '4': 0.75, '5': 1}
     return weight_ratio[value]
+
+
+def extract_criteria(source):
+    for question_key, question_content in source.items():
+        for criteria_key, criteria_value in question_content[0].items():
+            yield criteria_key, str(criteria_value)
+
+
+def export_ranking(alternative_names, ranking):
+    obj_list = [RankingResponse(alternative, rank).__dict__ for alternative, rank in zip(alternative_names, ranking)]
+    response = {"ranking": obj_list}
+    return response
+
+
+def update_criteria(source):
+    criteria = CRITERIA_DEFAULT
+    for i in source:
+        if i in CRITERIA_DEFAULT:
+            CRITERIA_DEFAULT[i] = source[i]
+    return criteria
 
 
 if __name__ == '__main__':
