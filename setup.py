069d1b1c02aecc93bd47e0c6d596f5140fa0f90c
Václav Stehlík <vaclav.stehlik@devix.cz>
2020-08-21 18:30:49 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='recommender',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Václav Stehlík',
+    author_email='',
+    description=''
+)
